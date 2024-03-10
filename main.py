dbc802224593010fae66a4b9abeee5285c96d4b7
niranjankumarnk <nkniranjankumar27@gmail.com>
2024-03-09 23:27:23 -0500

    Data ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from src.mlProject import logger
-logger.info("This is one of the execution")
+from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("C:/Users/Niranjan kumar/mldemo/config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
